f29d275121572451c82814e14b748e064fb0909f
Димас <gigakot@yandex.ru>
2022-03-19 21:10:03 +0300

    some improvements(i believe)

--- src/main/java/com/chartographer/ChartasController.java
+++ src/main/java/com/chartographer/ChartasController.java
@@ -22,24 +22,30 @@ import java.util.Base64;
 
 @Controller
 @RequestMapping("/chartas")
-public class FirstController {
+public class ChartasController {
 
     @Autowired
     private ChartaRepo chartaRepo;
 
     @GetMapping
-    public String getAll(Model model) {
+    public String readAll(Model model) {
         model.addAttribute("chartas", chartaRepo.findAll());
         return "index";
     }
 
     @GetMapping("/{id}")
-    public String getImg(@PathVariable Long id,
-                         @RequestParam Integer xCoord, @RequestParam Integer yCoord,
-                         @RequestParam Integer width, @RequestParam Integer height,
-                         Model model, HttpServletResponse response) throws IOException {
-        if (width > 5000 || height > 5000) {
+    public String read(@PathVariable Long id,
+                       @RequestParam Integer xCoord, @RequestParam Integer yCoord,
+                       @RequestParam Integer width, @RequestParam Integer height,
+                       Model model, HttpServletResponse response) throws IOException {
+        if (width > 5000 || height > 5000 || width <= 0 || height <= 0) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            model.addAttribute("chartas", chartaRepo.findAll());
+            return "index";
+        }
+        if (chartaRepo.findById(id).isEmpty()){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            model.addAttribute("chartas", chartaRepo.findAll());
             return "index";
         }
         Charta charta = chartaRepo.findById(id).get();
@@ -47,55 +53,50 @@ public class FirstController {
         BufferedImage chartaImg = ImageIO.read(new File(charta.getFileName()));
         BufferedImage pieceOfCharta = newCharta(width, height);
 
-
         if (xCoord + width > chartaImg.getWidth()) {
             width = chartaImg.getWidth() - xCoord;
         }
         if (yCoord + height > chartaImg.getHeight()) {
             height = chartaImg.getHeight() - yCoord;
         }
-
-        paint(width, height, pieceOfCharta, xCoord, yCoord, chartaImg);
-
-
-//        String path = "src/main/resources/tempchartas/" + "temp_" + charta.getId() + ".bmp";
-//        File file = new File(path);
-//        ImageIO.write(pieceOfCharta, "bmp", file);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                pieceOfCharta.setRGB(x, y, chartaImg.getRGB(x + xCoord, y + yCoord));
+            }
+        }
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
         ImageIO.write(pieceOfCharta, "bmp", outputStream);
-//        model.addAttribute("charta", outputStream);
-
         model.addAttribute("charta", Base64.getEncoder().encodeToString(outputStream.toByteArray()));
-
-
-//        model.addAttribute("charta", path);
-
-//        file.deleteOnExit();
-
-
-
         return "charta";
     }
 
     @PostMapping("/{id}")
-    public String update(@PathVariable("id") Long id, @RequestParam("file") MultipartFile multipartFile,
+    public String update(@PathVariable("id") Long id,@RequestBody @RequestParam("file") MultipartFile multipartFile,
                          @RequestParam Integer xCoord, @RequestParam Integer yCoord,
                          @RequestParam Integer width, @RequestParam Integer height,
                          Model model, HttpServletResponse response) throws IOException {
+        if (chartaRepo.findById(id).isEmpty()){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            model.addAttribute("chartas", chartaRepo.findAll());
+            return "index";
+        }
         Charta charta = chartaRepo.findById(id).get();
-
+        if (xCoord > charta.getWidth() || yCoord > charta.getHeight()
+                || xCoord < 0 || yCoord < 0
+                || width <= 0 || height <= 0
+        ) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            model.addAttribute("chartas", chartaRepo.findAll());
+            return "index";
+        }
         String path = "src/main/resources/chartas/" +
                 charta.getId() + "_" + charta.getWidth() + "_" + charta.getHeight() + ".bmp";
 
 
         ByteArrayInputStream bais = new ByteArrayInputStream(multipartFile.getBytes());
-
-
         BufferedImage base = ImageIO.read(new File(path));
         BufferedImage source = ImageIO.read(bais);
-
 
         if (xCoord + width > base.getWidth()) {
             width = base.getWidth() - xCoord;
@@ -103,20 +104,20 @@ public class FirstController {
         if (yCoord + height > base.getHeight()) {
             height = base.getHeight() - yCoord;
         }
-
-        paint(width, height, base, xCoord, yCoord, source);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                base.setRGB(xCoord + x, yCoord + y, source.getRGB(x, y));
+            }
+        }
 
         ImageIO.write(base, "bmp", new File(path));
-
         model.addAttribute("chartas", chartaRepo.findAll());
-
         return "index";
     }
 
     @PostMapping
-    public String create(@RequestParam int width, @RequestParam int height, Model model, HttpServletResponse response) {
-
-
+    public String create(@RequestParam Integer width, @RequestParam Integer height,
+                         Model model, HttpServletResponse response) throws IOException {
         if (width > 20_000 || height > 50_000
                 || width <= 0 || height <= 0) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -125,29 +126,18 @@ public class FirstController {
         }
 
         Charta charta = chartaRepo.save(new Charta(width, height));
-
         String path = "src/main/resources/chartas/" +
                 charta.getId() + "_" + charta.getWidth() + "_" + charta.getHeight() + ".bmp";
-
         charta.setFileName(path);
-
         chartaRepo.save(charta);
 
         BufferedImage img = newCharta(width, height);
+        ImageIO.write(img, "bmp", new File(path));
 
-        try {
-            ImageIO.write(img, "bmp", new File(path));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        model.addAttribute("chartas", chartaRepo.findAll());
-
-        response.addIntHeader("id", charta.getId().intValue());
         response.setStatus(HttpServletResponse.SC_CREATED);
+        model.addAttribute("chartas", chartaRepo.findAll());
         return "index";
     }
-
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Long id, Model model) {
@@ -161,19 +151,10 @@ public class FirstController {
         });
 
         model.addAttribute("chartas", chartaRepo.findAll());
-
         return "index";
     }
 
-
-    private void paint(Integer width, Integer height, BufferedImage base, Integer xCoord, Integer yCoord, BufferedImage source) {
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                base.setRGB(xCoord + x, yCoord + y, source.getRGB(x, y));
-            }
-        }
-    }
-
+    // создание новой харты
     private BufferedImage newCharta(int width, int height) {
         BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int x = 0; x < width; x++) {
@@ -183,5 +164,4 @@ public class FirstController {
         }
         return img;
     }
-
 }
--- src/main/java/com/chartographer/ChartographerApplication.java
+++ src/main/java/com/chartographer/ChartographerApplication.java
@@ -2,8 +2,6 @@ package com.chartographer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
-import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 public class ChartographerApplication {
